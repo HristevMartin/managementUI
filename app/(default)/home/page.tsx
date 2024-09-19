215857ae4b86f7945b3cdefee6ce20e8a3702cb3
martin hristev <hristevmartin96@gmail.com>
2024-09-19 12:58:38 +0100

    deleting unneeded files

--- app/(default)/home/page.tsx
+++
@@ -1,9 +0,0 @@
-const home = () => {
-    return (
-        <div>
-            <h1>Basic User View</h1>
-        </div>
-    )
-}
-
-export default home;
--- app/(default)/sample/page.tsx
+++
@@ -1,9 +0,0 @@
-const Sample = () => {
-    return (
-        <div className="border border-red-500">
-            something
-        </div>
-    )
-}
-
-export default Sample;
