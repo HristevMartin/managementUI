c885e0d1a67367c98a890fc78b22df59e9de4991
Martin Yankov Hristev <m.hristev@ubiquedigital.com>
2025-03-12 22:24:58 +0000

    temp

--- app/[locale]/backoffice/(default)/welcome-login/page.tsx
+++ app/[locale]/backoffice/(default)/welcome-login/page.tsx
@@ -6,14 +6,14 @@ const WelcomeLogin = ({ params }: any) => {
   const lang = params.locale;
   return (
     <div className="flex flex-col md:flex-row justify-center items-center h-screen w-screen p-8">
-      <div className="flex flex-col items-center border-b md:border-b-0 md:border-r border-gray-300 pb-6 md:pb-0 md:pr-12">
-        <h2 className="text-2xl md:text-3xl font-bold mb-6">Already have an account?</h2>
+      <div className="flex flex-col items-center border-b pb-6 md:pb-0 md:pr-12">
+        <h2 style={{ color: '#463bcf' }} className="text-2xl md:text-3xl font-bold mb-6">Already have an account?</h2>
         <Login lang={lang} />
       </div>
-      <div className="flex flex-col items-center pt-6 md:pt-0 md:pl-12 mb-6">
+      {/* <div className="flex flex-col items-center pt-6 md:pt-0 md:pl-12 mb-6">
         <h2 className="text-2xl md:text-3xl font-bold mb-6">Want to create an account?</h2>
         <Register lang={lang} />
-      </div>
+      </div> */}
     </div>
   )
 }
